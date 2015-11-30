f304b1836da93212e6676f722c4390de0ad450ea
Richard Livsey <richard@livsey.org>
2015-11-30 12:01:49 +0000

    Implement Mobiledoc parser for v0.3.0 format

--- src/js/parsers/mobiledoc/index.js
+++ src/js/parsers/mobiledoc/index.js
@@ -1,5 +1,8 @@
 import MobiledocParser_0_2 from './0-2';
-import { MOBILEDOC_VERSION } from 'mobiledoc-kit/renderers/mobiledoc/0-2';
+import MobiledocParser_0_3 from './0-3';
+
+import { MOBILEDOC_VERSION as MOBILEDOC_VERSION_0_2 } from 'mobiledoc-kit/renderers/mobiledoc/0-2';
+import { MOBILEDOC_VERSION as MOBILEDOC_VERSION_0_3 } from 'mobiledoc-kit/renderers/mobiledoc/0-3';
 
 function parseVersion(mobiledoc) {
   return mobiledoc.version;
@@ -9,8 +12,10 @@ export default {
   parse(builder, mobiledoc) {
     let version = parseVersion(mobiledoc);
     switch (version) {
-      case MOBILEDOC_VERSION:
+      case MOBILEDOC_VERSION_0_2:
         return new MobiledocParser_0_2(builder).parse(mobiledoc);
+      case MOBILEDOC_VERSION_0_3:
+        return new MobiledocParser_0_3(builder).parse(mobiledoc);
       default:
         throw new Error(`Unknown version of mobiledoc parser requested: ${version}`);
     }
